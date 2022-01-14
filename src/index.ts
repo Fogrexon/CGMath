bf031c06ce49519d29faec3f4029dc384509fab6
Fogrex <fogrexon@gmail.com>
2022-01-15 04:25:58 +0900

    add base files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export class Template {
-  constructor() {
-    console.log("log");
-  }
-}
+export * from './math/Matrix4';
+export * from './math/Quaternion';
+export * from './math/Vector2';
+export * from './math/Vector3';
+export * from './math/Vector4';
